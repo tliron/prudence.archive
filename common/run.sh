1e468e7232d6f7c2a71c04c764a7b43b208957cf
Tal Liron <tal.liron@gmail.com>
2009-11-17 06:05:37 +0000

    Initial support for flavors
    git-svn-id: https://prudence.googlecode.com/svn/trunk@37 333bf47c-70e5-882e-6e2c-8941cf9f370f

--- common/run.sh
+++ common/run.sh
@@ -8,12 +8,21 @@ main=com.threecrickets.scripturian.MainDocument
 jars=\
 lib/com.sun.grizzly.jar:\
 lib/com.sun.phobos.script.javascript.jar:\
+lib/com.caucho.quercus.jar:\
+lib/com.caucho.resin.util.jar:\
+lib/com.sun.script.jruby.jar:\
 lib/com.sun.script.velocity.jar:\
 lib/com.threecrickets.prudence.jar:\
 lib/com.threecrickets.scripturian.jar:\
 lib/javax.script.jar:\
+lib/javax.servlet.jar:\
+lib/jep.jar:\
+lib/jython.jar:\
+lib/jython-engine.jar:\
 lib/org.apache.log4j.jar:\
 lib/org.apache.velocity.jar:\
+lib/org.codehaus.groovy.jar:\
+lib/org.jruby.jar:\
 lib/org.json.jar:\
 lib/org.mozilla.javascript.jar:\
 lib/org.restlet.ext.grizzly.jar:\
@@ -24,4 +33,6 @@ lib/org.slf4j.bridge.jar:\
 lib/org.slf4j.impl.jar:\
 lib/org.slf4j.jar
 
-"$java" -cp "$jars" $main
+# We are setting java.library.path for Jepp
+
+"$java" -cp "$jars" -Djava.library.path=/usr/local/lib $main
