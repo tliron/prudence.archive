cc91ebe6203c9528e2f0c0def1823affaa4fb3a6
Tal Liron <tal.liron@gmail.com>
2010-05-01 23:24:41 +0000

    Quercus bins
    git-svn-id: https://prudence.googlecode.com/svn/trunk@447 333bf47c-70e5-882e-6e2c-8941cf9f370f

---
+++ php/bin/run.bat
@@ -0,0 +1,34 @@
+
+set java=java
+set main=com.threecrickets.scripturian.Scripturian
+
+set jars=libraries/com.caucho.quercus.jar;^
+libraries/com.mysql.jdbc.jar;^
+libraries/com.sun.grizzly.jar;^
+libraries/com.sun.script.velocity.jar;^
+libraries/com.threecrickets.jygments.jar;^
+libraries/com.threecrickets.prudence.jar;^
+libraries/com.threecrickets.scripturian.jar;^
+libraries/javax.script.jar;^
+libraries/javax.servlet.jar;^
+libraries/org.apache.commons.fileupload.jar;^
+libraries/org.apache.log4j.jar;^
+libraries/org.apache.velocity.jar;^
+libraries/org.codehaus.jackson.jar;^
+libraries/org.codehaus.jackson.mapper.jar;^
+libraries/org.h2.jar;^
+libraries/org.json.jar;^
+libraries/org.restlet.ext.fileupload.jar;^
+libraries/org.restlet.ext.grizzly.jar;^
+libraries/org.restlet.ext.jackson.jar;^
+libraries/org.restlet.ext.json.jar;^
+libraries/org.restlet.ext.slf4j.jar;^
+libraries/org.restlet.jar;^
+libraries/org.slf4j.bridge.jar;^
+libraries/org.slf4j.impl.jar;^
+libraries/org.slf4j.jar
+
+%java% ^
+-cp %jars% ^
+-Dscripturian.cache=cache ^
+%main% instance
