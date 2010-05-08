a20c8015e9afd1ff2c5ebfcdc6018f8cda12bab3
Tal Liron <tal.liron@gmail.com>
2010-05-08 07:22:37 +0000

    git-svn-id: https://prudence.googlecode.com/svn/trunk@474 333bf47c-70e5-882e-6e2c-8941cf9f370f

--- javascript/instance/component.js
+++ javascript/instance/component.js
@@ -10,5 +10,7 @@ importClass(
 
 // Create an H2-database-backed cache chained after the default memory cache
 var defaultCache = component.context.attributes.get('com.threecrickets.prudence.cache');
-var chainCache = new ChainCache(defaultCache, new H2Cache('cache/prudence/prudence'));
+var chainCache = new ChainCache();
+chainCache.caches.add(defaultCache);
+chainCache.caches.add(new H2Cache('cache/prudence/prudence'));
 component.context.attributes.put('com.threecrickets.prudence.cache', chainCache);
