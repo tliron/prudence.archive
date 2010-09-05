ae4ae5e230f089bbc89e81a873a10c007ad0487a
Tal Liron <tal.liron@gmail.com>
2010-09-05 08:37:29 +0000

    Add JUnit tasks to Ant build script
    git-svn-id: https://prudence.googlecode.com/svn/trunk@675 333bf47c-70e5-882e-6e2c-8941cf9f370f

--- java/com.threecrickets.prudence/src/com/threecrickets/prudence/test/internal/DistributionTest.java
+++ java/com.threecrickets.prudence/src/com/threecrickets/prudence/test/internal/DistributionTest.java
@@ -17,6 +17,8 @@ import static org.junit.Assert.assertTrue;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -37,7 +39,7 @@ public abstract class DistributionTest extends MultiTest
 	public DistributionTest( String name, boolean enabled )
 	{
 		// threads=40, iterations=1
-		super( enabled ? 0 : 0, enabled ? 1 : 0 );
+		super( enabled ? 10 : 0, enabled ? 1 : 0 );
 		this.name = name;
 	}
 
@@ -101,6 +103,8 @@ public abstract class DistributionTest extends MultiTest
 				x.printStackTrace();
 			}
 		}
+
+		Logger.getLogger( "" ).setLevel( Level.WARNING );
 	}
 
 	@AfterClass
