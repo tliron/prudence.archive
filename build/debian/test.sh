f64e00b75ee0b0070dc58967aa52dd6390b9362d
Tal Liron <tal.liron@gmail.com>
2010-09-16 23:43:21 +0000

    Work work on Debian packaging
    git-svn-id: https://prudence.googlecode.com/svn/trunk@719 333bf47c-70e5-882e-6e2c-8941cf9f370f

--- build/debian/test.sh
+++ build/debian/test.sh
@@ -2,8 +2,17 @@
 
 HERE=$(readlink -f "$(dirname "$0")")
 
-cd $HERE/prudence-clojure-1.0
+cd $HERE
+rm *.changes
+rm *.deb
+rm *.dsc
+rm *.tar.gz
+
+cd prudence-clojure-1.0
 rm files
 rm prudence-clojure.debhelper.log
 rm prudence-clojure.substvars
 dpkg-buildpackage
+
+cd ..
+sudo dpkg -i prudence-clojure*.deb
