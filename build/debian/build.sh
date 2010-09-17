5c454eca80c9b560b2a24b85e2eb0c5345cc000d
Tal Liron <tal.liron@gmail.com>
2010-09-17 04:32:23 +0000

    More work on Debian packaging
    git-svn-id: https://prudence.googlecode.com/svn/trunk@733 333bf47c-70e5-882e-6e2c-8941cf9f370f

--- build/debian/build.sh
+++ build/debian/build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+set -e
+
+DIST=$1
+
+if [ -z "$DIST" ]; then
+	echo Must supply distribution name
+	exit 1
+fi
 
 HERE=$(readlink -f "$(dirname "$0")")
 
-cd $HERE/prudence-clojure-1.0
+cd $HERE/prudence-$DIST-1.0
+rm -rf content
+cp -r ../../$DIST/content .
 dpkg-buildpackage -S -kC11D6BA2
--- build/debian/test.sh
+++ build/debian/test.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
+set -e
 
 DIST=$1
+
+if [ -z "$DIST" ]; then
+	echo Must supply distribution name
+	exit 1
+fi
+
 HERE=$(readlink -f "$(dirname "$0")")
 
 cd $HERE
-rm prudence-$DIST*.changes
-rm prudence-$DIST*.deb
-rm prudence-$DIST*.dsc
-rm prudence-$DIST*.tar.gz
+rm -f prudence-$DIST*.changes
+rm -f prudence-$DIST*.deb
+rm -f prudence-$DIST*.dsc
+rm -f prudence-$DIST*.tar.gz
 
 cd prudence-$DIST-1.0
-rm files
-rm prudence*.debhelper.log
-rm prudence*.substvars
-dpkg-buildpackage
+rm -f files
+rm -f prudence*.debhelper.log
+rm -f prudence*.substvars
+
+rm -rf content
+cp -r ../../$DIST/content .
+dpkg-buildpackage -b
 
 cd ..
-sudo dpkg -i prudence-${DIST}*.deb
+#sudo dpkg -i prudence-${DIST}*.deb
