a2500d4d0d0d35670234133a8bf45edab4e1d8ab
Tal Liron <tal.liron@gmail.com>
2010-09-20 07:06:15 +0000

    More work on Debian packaging
    git-svn-id: https://prudence.googlecode.com/svn/trunk@739 333bf47c-70e5-882e-6e2c-8941cf9f370f

--- build/debian/test.sh
+++ build/debian/test.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#
+# Copyright 2009-2010 Three Crickets LLC.
+#
+# The contents of this file are subject to the terms of the LGPL version 3.0:
+# http://www.opensource.org/licenses/lgpl-3.0.html
+#
+# Alternatively, you can obtain a royalty free commercial license with less
+# limitations, transferable or non-transferable, directly from Three Crickets
+# at http://threecrickets.com/
+#
+
 set -e
 
 DIST=$1
@@ -11,15 +22,15 @@ fi
 HERE=$(cd "${0%/*}" 2>/dev/null; echo "$PWD")
 
 cd $HERE
-rm -f prudence-$DIST*.changes
-rm -f prudence-$DIST*.deb
-rm -f prudence-$DIST*.dsc
-rm -f prudence-$DIST*.tar.gz
+#rm -f prudence-$DIST*.changes
+#rm -f prudence-$DIST*.deb
+#rm -f prudence-$DIST*.dsc
+#rm -f prudence-$DIST*.tar.gz
 
 cd prudence-$DIST-1.0
-rm -f files
-rm -f prudence*.debhelper.log
-rm -f prudence*.substvars
+#rm -f files
+#rm -f prudence*.debhelper.log
+#rm -f prudence*.substvars
 
 rm -rf content
 cp -r ../../$DIST/content .
