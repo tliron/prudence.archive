4f40184177075f18b8a10bf68b0e29e3f786c2d1
Tal Liron <tal.liron@gmail.com>
2012-03-20 23:28:45 -0500

    Update copyright tag

--- components/flavors/php/defaults/application/default.php
+++ components/flavors/php/defaults/application/default.php
@@ -5,7 +5,7 @@
 // Copyright 2009-2011 Three Crickets LLC.
 //
 // The contents of this file are subject to the terms of the LGPL version 3.0:
-// http://www.opensource.org/licenses/lgpl-3.0.html
+// http://www.gnu.org/copyleft/lesser.html
 //
 // Alternatively, you can obtain a royalty free commercial license with less
 // limitations, transferable or non-transferable, directly from Three Crickets
