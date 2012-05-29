76c2238a0e9e7a9ebbb70d04cdd6c0a96ed42a53
Tal Liron <tal.liron@gmail.com>
2012-05-28 21:03:02 -0500

    Configure Hazelcast by script

--- components/distributed/component/services/distributed/default.js
+++ components/distributed/component/services/distributed/default.js
@@ -1,7 +1,36 @@
 
-importClass(java.lang.System)
+document.execute('/sincerity/container/')
 
-System.setProperty('hazelcast.config', String(sincerity.container.getConfigurationFile('hazelcast.conf')))
-try {
-	com.hazelcast.core.Hazelcast.defaultInstance
-} catch (x) {}
+var config = sincerity.container.getConfigurationFile('hazelcast.conf')
+if (config.exists()) {
+	java.lang.System.setProperty('hazelcast.config', String(config))
+	try {
+		// Initializes from system property 
+		com.hazelcast.core.Hazelcast.defaultInstance
+	} catch (x) {}
+}
+else
+{
+	importClass(
+		com.hazelcast.config.Config,
+		com.hazelcast.config.MapConfig,
+		com.hazelcast.config.MultiMapConfig,
+		com.hazelcast.config.QueueConfig,
+		com.hazelcast.config.TopicConfig,
+		com.hazelcast.config.SemaphoreConfig,
+		com.hazelcast.config.MergePolicyConfig,
+		com.hazelcast.config.WanReplicationConfig,
+		com.hazelcast.config.ExecutorConfig,
+		com.hazelcast.config.NetworkConfig,
+		com.hazelcast.config.ListenerConfig,
+		com.hazelcast.config.SecurityConfig,
+		com.hazelcast.config.ManagementCenterConfig,
+		com.hazelcast.config.PartitionGroupConfig,
+		com.hazelcast.core.Hazelcast)
+	
+	var config = new Config()
+	
+	Sincerity.Container.executeAll(sincerity.container.getConfigurationFile('hazelcast'))
+
+	Hazelcast.init(config)
+}
---
+++ components/distributed/configuration/hazelcast/prudence/default.js
@@ -0,0 +1,19 @@
+
+var map = new MapConfig()
+map.name = 'com.threecrickets.prudence.cache'
+map.backupCount = 1
+map.evictionPolicy = 'LFU'
+map.evictionPercentage = 25
+config.addMapConfig(map)
+
+map = new MapConfig()
+map.name = 'com.threecrickets.prudence.cacheTagMap'
+map.backupCount = 1
+config.addMapConfig(map)
+
+map = new MapConfig()
+map.name = 'com.threecrickets.prudence.distributedGlobals'
+map.backupCount = 1
+map.evictionPolicy = 'LFU'
+map.evictionPercentage = 25
+config.addMapConfig(map)
